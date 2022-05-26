e87bd3be45e490354f25d23e38e5058a6d2b4bd4
kakkurt47 <93423375+kakkurt47@users.noreply.github.com>
2022-05-26 16:47:54 +0300

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -198,8 +198,8 @@ async def play(_, message: Message):
             keyboard = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("📚 Bilgi", url=f"https://t.me/Suskunlarkanali"),
-                InlineKeyboardButton("📝 Grub", url=f"https://t.me/Suskunlarsohbetonline"),
+                InlineKeyboardButton("📚 Bilgi", url=f"https://t.me/yangazlargrup"),
+                InlineKeyboardButton("📝 Grub", url=f"https://t.me/yangazlargrup"),
             ],[
                 InlineKeyboardButton("✨ Kapat", callback_data="cls"),
             ],
@@ -263,8 +263,8 @@ async def play(_, message: Message):
         keyboard = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("📚 Bilgi", url=f"https://t.me/Suskunlarkanali"),
-                InlineKeyboardButton("📝 Grub", url=f"https://t.me/Suskunlarsohbetonline"),
+                InlineKeyboardButton("📚 Bilgi", url=f"https://t.me/yangazlargrup"),
+                InlineKeyboardButton("📝 Grub", url=f"https://t.me/yangazlargrup"),
             ],[
                 InlineKeyboardButton("✨ Kapat", callback_data="cls"),
             ],
